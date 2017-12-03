49ac9203befa96f1bf9afbc079fa52758b0c05cc
Arvind Sharma <avin001@gmail.com>
2017-12-03 23:29:50 +0530

    Added new file SomeBuiltInFunctions.py
    Added new file SomeBuiltInFunctions.py

---
+++ SomeBuiltInFunctions.py
@@ -0,0 +1,10 @@
+udacity_length = len("Udacity")
+print(udacity_length)
+given_name = "John"
+middle_name = "William"
+family_name = "Ferrell"
+full_name = given_name + " " + middle_name + " " + family_name
+name_length = len(full_name)
+print(name_length)
+driving_license_character_limit = 28
+print(name_length <= driving_license_character_limit)
