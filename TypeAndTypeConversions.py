6bb5cd32f2a1b95b554dc34e5d34d89a949bf65a
Arvind Sharma <avin001@gmail.com>
2017-12-04 01:22:49 +0530

    Added new files SomeBuiltInFunctions.py and TypeAndTypeConversions.py
    Added new files SomeBuiltInFunctions.py and TypeAndTypeConversions.py

---
+++ TypeAndTypeConversions.py
@@ -0,0 +1,14 @@
+count = int(4.0)
+print(count)
+print(type(count))
+
+house_number = 13
+street_name = "The Crescent"
+town_name = "Belmont"
+print(type(house_number))
+address = str(house_number) + " " + street_name + ", " + town_name
+print(address)
+
+grams_of_sugar = float("35.0")
+print(grams_of_sugar)
+print(type(grams_of_sugar))
