4092e3b675e22be316cdcf3d61c22db64cff1ae5
Arvind Sharma <avin001@gmail.com>
2017-12-03 14:54:11 +0530

    Added new file VariablesAgain.py
    Added new file VariablesAgain.py

---
+++ VariablesAgain.py
@@ -0,0 +1,16 @@
+savings = 514.86
+salary = 320.51
+print(savings)
+print(salary)
+savings, salary = 4578.75, 1256.45
+print(savings)
+print(salary)
+
+manila_pop = 1780148
+manila_area = 16.56
+manila_pop_density = manila_pop / manila_area
+print(int(manila_pop_density))
+manila_pop = 1781573
+print(int(manila_pop_density))
+manila_pop_density = manila_pop / manila_area
+print(int(manila_pop_density))
