d2de3d4a1335569b307fbfc227653d75cc1b53b7
Arvind Sharma <avin001@gmail.com>
2017-12-03 22:57:37 +0530

    Added new files HelloComparisionOperators.py and StringAndText.py
    Added new files HelloComparisionOperators.py and StringAndText.py

---
+++ HelloComparisionOperators.py
@@ -0,0 +1,10 @@
+the_sun_is_up = True
+the_sky_is_blue = False
+print(1 < 2)
+print(42 > 43)
+
+sf_population, sf_area = 864816, 231.89
+rio_population, rio_area = 64533682, 486.5
+san_francisco_pop_density = sf_population / sf_area
+rio_de_janeiro_pop_desnity = rio_population / rio_area
+print(san_francisco_pop_density > rio_de_janeiro_pop_desnity)
---
+++ StringsAndText.py
@@ -0,0 +1,25 @@
+print('Hello.')
+print("Hi there.")
+welcome_message = 'Hello, welcome to Udacity!'
+print(welcome_message)
+intro_message = "This is Python Foundation Nanodegree program."
+print(intro_message)
+instructor_1 = 'Philip'
+instructor_2 = 'Charlie'
+print(instructor_1 + instructor_2)
+print(instructor_1 + " and " + instructor_2)
+pet_halibut = 'Why should I be tarred with the epithet "loony" merely because I have a pet halibut?'
+print(pet_halibut)
+salesman = '"I think you\'re an encyclopaedia salesman."'
+print(salesman)
+question = "What's the meeting agenda?"
+print(question)
+coconut_count = "34"
+mango_count = "15"
+tropical_fruit_count = coconut_count + mango_count
+print(tropical_fruit_count)
+username = 'Kinari'
+timestamp = '04:50'
+url = 'http://petshop.com/pets/mammals/cats'
+message = username + ' accessed the site ' + url + ' at ' + timestamp + '.'
+print(message)
