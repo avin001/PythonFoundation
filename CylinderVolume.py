ff34c2dfe7b1e3e87a280cb96f4d0c81930bd0b4
Arvind Sharma <avin001@gmail.com>
2017-12-04 05:43:13 +0530

    Added new file CylinderVolume.py
    Added new file CylinderVolume.py

---
+++ CylinderVolume.py
@@ -0,0 +1,13 @@
+def cylinder_volume(height, radius):
+    pi = 3.14159
+    return height * pi * (radius ** 2)
+
+
+print(cylinder_volume(10, 5))
+
+
+def print_greeting():
+    print("Hello world!")
+
+
+print_greeting()
