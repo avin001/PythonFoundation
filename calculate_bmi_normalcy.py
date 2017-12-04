f93f18cc36bb07ca7125fe267507899af3503a21
Arvind Sharma <avin001@gmail.com>
2017-12-04 16:35:07 +0530

    Updated file calculate_bmi_normalcy.py
    Updated file calculate_bmi_normalcy.py

--- calculate_bmi_normalcy.py
+++ calculate_bmi_normalcy.py
@@ -1,6 +1,8 @@
 def is_bmi_normal(weight_in_kg, height_in_m):
     if 18.5 <= weight_in_kg / (height_in_m)**2 < 25:
         print("Your BMI is considered 'normal'.")
+    else:
+        print("Your BMI is not considered 'normal'.")
 
 
-is_bmi_normal(70, 1.7)
+is_bmi_normal(75, 1.8)
