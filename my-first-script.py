4b2bfa9827cb465e2e26b1d6f4824d6f7b3964da
Arvind Sharma <avin001@gmail.com>
2017-12-04 14:24:47 +0530

    Updated file my-first-script.py
    Updated file my-first-script.py

--- my-first-script.py
+++ my-first-script.py
@@ -1,7 +1,9 @@
 def welcome_message(name):
-#Prints out a personalised welcome message
-    return "Welcome to this Python script, " + name + "!"
+    # Prints out a personalised welcome message
+    return "How has been your experience with Python so far, " + name + "?"
 
-#Call the welcome message function with the input "Udacity Student" 
-#and print the output
-print(welcome_message("Udacity Student"))
+
+# Call the welcome message function with the input "Udacity Student"
+# and print the output
+print(welcome_message("Avin"))
+print("It has been amazing!")
