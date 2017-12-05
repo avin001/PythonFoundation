f897e79d30d8d3c66e13e8a25e11fa7eb097d7bf
Arvind Sharma <avin001@gmail.com>
2017-12-06 02:38:05 +0530

    Added new file working_with_lists.py
    Added new file working_with_lists.py

---
+++ working_with_lists.py
@@ -0,0 +1,25 @@
+week_list = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+print(len(week_list))
+print(max(week_list))
+print(min(week_list))
+print(sorted(week_list))
+print(week_list)
+print(sorted(week_list, reverse = True))
+print(week_list)
+batch_sizes = [15, 6, 89, 34, 65, 35]
+print(len(batch_sizes))
+print(max(batch_sizes))
+print(min(batch_sizes))
+print(sorted(batch_sizes))
+print(batch_sizes)
+print(sorted(batch_sizes, reverse = True))
+print(batch_sizes)
+
+nautical_directions = "\n".join(["fore", "aft", "starboard", "port"])
+print(nautical_directions)
+names = ["Garcia", "O'Kelly", "Davis"]
+print("-".join(names))
+
+programming_languages = ['Java', 'Python', 'C#']
+programming_languages.append('C++')
+print(programming_languages)
