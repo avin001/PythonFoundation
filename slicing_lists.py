c65e1904794b85007e99eb0f6679214b1156cf9f
Arvind Sharma <avin001@gmail.com>
2017-12-05 22:40:23 +0530

    Added new file slicing_lists.py
    Added new file slicing_lists.py

---
+++ slicing_lists.py
@@ -0,0 +1,8 @@
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+quater3 = months[6:9]
+print(quater3)
+print(months)
+first_half = months[:6]
+print(first_half)
+second_half = months[6:]
+print(second_half)
