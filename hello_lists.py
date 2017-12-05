a8d95eb530b1c97e77f1c3d901632aecccb2dc61
Arvind Sharma <avin001@gmail.com>
2017-12-05 22:01:37 +0530

    Added new file hello_lists.py
    Added new file hello_lists.py

---
+++ hello_lists.py
@@ -0,0 +1,11 @@
+python_versions = [1.0, 1.5, 1.6, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6]
+print(python_versions[0])
+print(python_versions[1])
+print(python_versions[7])
+print(python_versions[-1])
+print(python_versions[-2])
+print(python_versions[-3])
+
+my_list = ['a', 'b', 'c', 'd', 'e']
+print(my_list[4])
+print(my_list[-1])
