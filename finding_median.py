27dcb57effc165fa424232b439062bef8b3a4289
Arvind Sharma <avin001@gmail.com>
2017-12-06 15:59:24 +0530

    Added new file finding_median.py
    Added new file finding_median.py

---
+++ finding_median.py
@@ -0,0 +1,8 @@
+def median(numbers):
+    numbers.sort()
+    if len(numbers) % 2 != 0:
+        return numbers[int(len(numbers) / 2)]
+    else:
+        return (numbers[int(len(numbers) / 2) - 1] + numbers[int(len(numbers) / 2)]) / 2
+
+print(median([1, 2, 3, 4]))
