1d125a70f808db2c400bc6ef440175a2ada78400
Arvind Sharma <avin001@gmail.com>
2017-12-06 17:21:43 +0530

    Added new file for_loops.py
    Added new file for_loops.py

---
+++ for_loops.py
@@ -0,0 +1,16 @@
+names = ['charlotte hippopotamus turner', 'oliver st. john-mollusc', 'nigel incubator-jones', 'philip diplodocus mallory']
+for name in names:
+    print(name.title())
+
+def list_sum(input_list):
+    sum = 0
+    for item in input_list:
+        sum += int(item)
+    return sum
+
+
+test1 = list_sum([1, 2, 3])
+print("expected result: 6, actual result: {}".format(test1))
+
+test2 = list_sum([-1, 0 , 1])
+print("expected result: 0, actual result: {}".format(test2))
