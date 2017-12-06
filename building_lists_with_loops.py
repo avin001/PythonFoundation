c7278934dfbed214d5bb05b644759e196d3ce1d7
Arvind Sharma <avin001@gmail.com>
2017-12-06 18:00:08 +0530

    Added new file building_lists_with_loops.py
    Added new file building_lists_with_loops.py

---
+++ building_lists_with_loops.py
@@ -0,0 +1,10 @@
+names = ['charlotte hippopotamus turner', 'oliver st. john-mollusc', 'nigel incubator-jones', 'philip diplodocus mallory']
+capitalized_names = []
+for name in names:
+    capitalized_names.append(name.title())
+print(names)
+print(capitalized_names)
+
+for index in range(len(names)):
+    names[index] = names[index].title()
+print(names)
