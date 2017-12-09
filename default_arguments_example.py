5ca697608e1f373ef72e4526727bcfefe6f2c11d
Arvind Sharma <avin001@gmail.com>
2017-12-10 00:31:45 +0530

    Updated the file default_arguments_example.py as per PEP8 guidelines
    Updated the file default_arguments_example.py as per PEP8 guidelines

--- default_arguments_example.py
+++ default_arguments_example.py
@@ -1,14 +1,14 @@
-def box(width, height, symbol = '*'):
+def box(width, height, symbol='*'):
     """Print a box made up of asterisks or some other character.
     width: width of the box in characters, must be at least 2
     height: height of box in linex, must be at least 2
     symbol: a single character string used to draw the box edges
     """
-    print(symbol * width) # print top edge of the box
+    print(symbol * width)  # print top edge of the box
     # print sides of the box
     for _ in range(height - 2):
         print(symbol + " " * (width - 2) + symbol)
-    print(symbol * width) # print bottom edge of the box
+    print(symbol * width)  # print bottom edge of the box
 
 
 box(7, 5)
