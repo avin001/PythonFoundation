4f091d446400722057df77b33fad896252095928
Arvind Sharma <avin001@gmail.com>
2017-12-10 00:57:20 +0530

    Updated the file stopping_for_a_break.py as per PEP8 guidelines
    Updated the file stopping_for_a_break.py as per PEP8 guidelines

--- stopping_for_a_break.py
+++ stopping_for_a_break.py
@@ -1,5 +1,5 @@
 # each item in the manifest is an item and its weight
-manifest =[["bananas", 15], ["mattresses", 34], ["dog kennels", 42], ["machine that goes ping!", 120], ["tea chests", 10], ["cheeses", 0]]
+manifest = [["bananas", 15], ["mattresses", 34], ["dog kennels", 42], ["machine that goes ping!", 120], ["tea chests", 10], ["cheeses", 0]]
 cargo_weight = 0
 cargo_hold = []
 for cargo in manifest:
